87a60daa296a46ff30aae56e4f06735f67547d94
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-11-05 11:11:07 +0500

    add image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 export default class App extends Component {
   render() {
@@ -11,10 +11,23 @@ export default class App extends Component {
         <View style={styles.two}>
           <Text style={styles.text2}>Make your own private</Text>
           <Text style={styles.text3}> travel plan</Text>
+          <Text style={styles.text4}>Formulate your strategy to receive wonderful gift packs</Text>
         </View>
         <View style={styles.three}>
-          <Text>Formulate your strategy to receive wonderful gift packs</Text>
+          {/* <Text style={styles.text4}>Formulate your strategy to receive wonderful gift packs</Text> */}
         </View>
+
+
+        <TouchableOpacity style={styles.four}>
+          <View style={styles.button}>
+            <Image source={require('./src/assets/Arrow.png')} />
+          </View>
+        </TouchableOpacity>
+
+        <View style={styles.five}>
+         <Text style={styles.text5}>Nordic Vacation Sponsor</Text>
+        </View>
+
       </View>
     );
   }
@@ -70,5 +83,45 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 12,
     alignItems: 'center',
+  }, text4: {
+    paddingTop: 45,
+    flex: 1,
+    alignItems: 'center',
+    color: '#B4B4B4', // Text color in React Native
+    fontFamily: 'Poppins', // Use a valid font family if it's available on your device
+    fontSize: 18, // Font size in device-independent pixels (dp)
+    fontStyle: 'normal', // Font style
+    fontWeight: '400', // Font weight
+    lineHeight: 22, // Line height in dp (adjust as needed)
+    letterSpacing: -0.27, // Letter spacing (adjust as needed)
+    textAlign: 'center', // Text alignment
+  },
+ 
+  four: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#0373F3',
+    width: 76,
+    height: 76,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  five:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  text5:{
+    color: '#595959',
+    fontFamily: 'Poppins',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 20, 
   }
 });
